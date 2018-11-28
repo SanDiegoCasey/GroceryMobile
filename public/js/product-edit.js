31d0ba459db5ad4f16ce16e204e168926793bfc6
Casey Whitcher <sandiegocasey@gmail.com>
2018-11-28 16:51:52 -0600

    cleaned up CSS and fixed calculator on edit page

--- public/js/product-edit.js
+++ public/js/product-edit.js
@@ -86,5 +86,82 @@ $(function() {
     });
 
   });
+  // Calculator Section
+  $('.submit-calc').click(function(e) {
+    let selected = $('#conversion-selector option:selected').val();
+    let qty = $('#unitQty').val();
+    let prc = $('#price').val();
+    let perOz = (prc / qty).toFixed(2);
+    let perLb = ((prc / qty) * 16).toFixed(2);
+    let perLbByLb = (prc / qty).toFixed(2);
+    let perOzByLb = (prc / (qty * 16)).toFixed(2);
+    let perLtByLt = (prc / qty).toFixed(2);
+    let perGaByLt = (prc / (qty * .2641720524)).toFixed(2);
+    let perGaByOz = ((prc / qty) * 128).toFixed(2);
+    let perGaByQt = (prc / (qty / 4)).toFixed(2);
+    let perQtByQt = (prc / qty).toFixed(2);
+    let perQtByLt = (prc / (qty / .946353)).toFixed(2);
+    let perLtByQt = (prc / (qty * .946353)).toFixed(2);
+    let perEaByEa = (prc / qty).toFixed(2);
+    let perGaByGa = (prc / qty).toFixed(2);
+    let perQtByGa = (prc / (qty * 4)).toFixed(2);
+    let perLtByGa = (prc / ((qty * 4) * .946353)).toFixed(2);
+    let perOzByGa = (prc / (qty * 128)).toFixed(2);
+    let perLtByOz = (prc / (qty / 33.814)).toFixed(2);
+    let perQtByOz = (prc / (qty / 32)).toFixed(2);
+
+
+    e.preventDefault();
+    document.getElementById('unit-result').innerHTML = '';
+    if (selected === 'oz') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perOz}</span> per ounce<br>\
+      <span class="computed-price">$${perLb}</span> per pound<br>\
+      <span class="computed-price">$${perLtByOz}</span> per Liter<br>\
+      <span class="computed-price">$${perQtByOz}</span> per Quart<br>\
+      <span class="computed-price">$${perGaByOz}</span> per Gallon<br>`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+    } else if (selected === 'lbs') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perLbByLb}</span> per pound<br>\
+        <span class="computed-price">$${perOzByLb}</span> per ounce`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+      // return false;
+    } else if (selected === 'each') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perEaByEa}</span> per each`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+      // return false;
+    } else if (selected === 'Liter') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perLtByLt}</span> per liter<br>\
+      <span class="computed-price">$${perQtByLt}</span> per Quart<br>\
+        <span class='computed-price'>$${perGaByLt}</span> per Gallon`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+      // return false;
+    } else if (selected === 'Quart') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perQtByQt}</span> per Quart<br>\
+      <span class="computed-price">$${perGaByQt}</span> per Gallon<br>\
+        <span class='computed-price'>$${perLtByQt}</span> per Liter`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+      // return false;
+    }else if (selected === 'Gallon') {
+      document.getElementById('unit-result').innerHTML = `<span class="computed-price">$${perGaByGa}</span> per Gallon<br>\
+        <span class="computed-price">$${perQtByGa}</span> per Quart<br>\
+        <span class='computed-price'>$${perLtByGa}</span> per Liter<br>\
+        <span class='computed-price'>$${perOzByGa}</span> per Ounce`;
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, 'slow');
+      // return false;
+    }
+
+  });
 
 });
